38d0eba09367e292a545676bc3a8d308a07cd3cd
Anderson Santo <kalangoti@gmail.com>
2021-11-08 18:46:52 -0300

    Backend project created

---
+++ backend/src/test/java/com/kti/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kti.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
