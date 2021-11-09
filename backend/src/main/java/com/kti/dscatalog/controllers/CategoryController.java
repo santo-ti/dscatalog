7fd41048ec2a653bd85afb7f16c0b01b84f79268
Anderson Santo <kalangoti@gmail.com>
2021-11-08 23:11:41 -0300

    Category controller

---
+++ backend/src/main/java/com/kti/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package com.kti.dscatalog.controllers;
+
+import com.kti.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electronics"));
+        return ResponseEntity.ok(list);
+    }
+}
