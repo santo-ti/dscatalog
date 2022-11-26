9564f2549cbda4ac6c9d8da339d7d0b56dcd556b
Anderson Santo <kalangoti@gmail.com>
2022-11-26 00:36:21 -0300

    Add DTO

--- backend/src/main/java/com/kti/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/kti/dscatalog/controllers/CategoryController.java
@@ -1,6 +1,6 @@
 package com.kti.dscatalog.controllers;
 
-import com.kti.dscatalog.entities.Category;
+import com.kti.dscatalog.dto.CategoryDTO;
 import com.kti.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +18,8 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok(list);
     }
 }
--- backend/src/main/java/com/kti/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/kti/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.kti.dscatalog.services;
 
+import com.kti.dscatalog.dto.CategoryDTO;
 import com.kti.dscatalog.entities.Category;
 import com.kti.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,7 +16,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }
